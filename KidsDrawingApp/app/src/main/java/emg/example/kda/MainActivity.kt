d084ff1e497d5b62f6bd399fd8218ebbd324f3f5
petrubear <edison.martinez@gmail.com>
2022-10-02 14:42:17 -0500

    feat: share image

--- KidsDrawingApp/app/src/main/java/emg/example/kda/MainActivity.kt
+++ KidsDrawingApp/app/src/main/java/emg/example/kda/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -229,6 +230,7 @@ class MainActivity : AppCompatActivity() {
                                 this@MainActivity, "File saved successfully: $result",
                                 Toast.LENGTH_SHORT
                             ).show()
+                            shareImage(result)
                         } else {
                             Toast.makeText(
                                 this@MainActivity, "Something went wrong while saving the file.",
@@ -264,5 +266,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
         customProgressDialog = null
+    }
+
+    private fun shareImage(result: String) {
+        MediaScannerConnection.scanFile(this, arrayOf(result), null) { path, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+            startActivity(Intent.createChooser(shareIntent, "Share"))
+        }
     }
 }
